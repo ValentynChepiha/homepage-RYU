ab388d983742e8b0555c5294b585f696825f44a7
ValentynChepiha <valentin.chepiga@gmail.com>
2020-05-04 18:25:33 +0300

    Added secondary content (e.g. languages, skills, character, etc.)

---
+++ js/content-en.js
@@ -0,0 +1,143 @@
+const
+  resumeRyuEn = {
+    'titles' : {
+      'experience' : 'EXPERIENCE',
+      'publications' : 'PUBLICATIONS',
+      'achievements' : 'ACHIEVEMENTS',
+      'languages' : 'Languages',
+      'skills' : 'SKILLS',
+      'character' : 'CHARACTER',
+      'lifeLesson' : 'LIFE LESSON',
+      'lifeEducation' : 'LIFE & EDUCATION',
+      'likes' : 'LIKES',
+      'dislikes' : 'DISLIKES',
+      'wants' :  'WANTS',
+    },
+    'experience' : [
+      {
+        'year': 2018,
+        'comment': 'Amongst many fighters of the past was featured in the movie "Ready Player One". Ryu ready always!'
+      },
+      {
+        'year': 2017,
+        'comment':  'United with unknown until then to me Power Rangers to defea resurrected M.Bison.Damn good fighters they are, even not knowing karate ways!'
+      },
+      {
+        'year': 2016,
+        'commnet': 'In "Street Fighter V" once and for all erased M.Bison from earth and tamed my inner Evil Ryu. Also spent some time on a side-project, mini-series "Street Fighter: Resurrection".'
+      },
+      {
+        'year': 2014,
+        'comment': 'Filmed in a martial arts tutorial "Street Fighter: Assassin\'s Fist", revealing to audience forgotten art of Ansatsuken.'
+      },
+      {
+        'year': 2012,
+        'comment': 'The gods of intenet concluded me in the animated movie "Wreck-It Ralph", but rewarded me with a good drink for it!'
+      },
+      {
+        'year': 2008,
+        'comment': 'My evil alter ego sneaked out of control and became a boss in "Street Fighter IV".'
+      },
+      {
+        'year': 2006,
+        'comment': 'Borrowed my famous ripped gi to Soki in "Onimusha: Dawn of Dreams", gods informed me he needed that for the battle.'
+      },
+      {
+        'year': 1998,
+        'comment': 'Defeated M. Bison in "Street Fighter Alpha 2", bastard wanted to use my heroic body for his evil intentions, but retreated with shame. Also this year I encountered Marvel universe in "Marvel vs. Capcom: Clash of Super Heroes" fighter,loyal to my employer, Capcom, of course.'
+      },
+      {
+        'year': 1997,
+        'comment': 'Reunited with Ken, my friend and opponent in "Street Fighter III". Men\'s friendship works mysteriously. Also helped with Arika project "Street Fighter EX"'
+      },
+      {
+        'year': 1996,
+        'comment': 'Took care of villainous Akuma in "Street Fighter Alpha 2". As a freelance on "Super Puzzle Fighter II Turbo" tried myself in a new genre, puzzle games. Let the wrath of seven heavens come upon these malicious enigmas, never again!'
+      },
+      {
+        'year': 1995,
+        'comment': 'Kept fighting in "Street Fighter Alpha: Warriors\' Dreams".'
+      },
+      {
+        'year': 1994,
+        'comment': 'Screened in a movie "Street Fighter", so that the batllesof past would not be forgotten by millenial generation.'
+      },
+      {
+        'year': 1991,
+        'comment': 'Was promoted to "Street Fighter II". Didn\'t stay there for long, celebrations and office dwelling is not for me!'
+      },
+      {
+        'year': 1987,
+        'comment': 'Started working for Capcom project "Street Fighter". My partner was Ken Masters, the boss\'name was Sagat.'
+      }
+    ],
+
+    'publications' : [
+      {
+        'year': '2011',
+        'comment': 'The true and humble story of me under pseudonim Tyler Wilde in an article "The evolution of Ken and Ryu"'
+      },
+      {
+        'year': '2003-2018',
+        'comment': 'For 15 years I have published comic books with Udon Entertainment depicting my adventures, bitter lessons and great victories.'
+      }
+    ],
+
+    'achievements' : [
+      '#1 in "Top 25 Street Fighter Characters" by IGN, who also called me "fancy", "icon" and "a testament to the virtue of simplicity',
+      '#2 in "Top 20 Street Fighter Characters of All Time" by GameDaily, same council found me worth of #6 place in "Top 25 Capcom Characters of All Time", which I proudly share with my partner Ken I was named "5th Most Powerful Street Fighter Character" by Screen Rant-san',
+      '#71 in "Top 100 Heroes of All Time" by UGO Networks, also recognized #2 in their list of "Top 50 Street Fighter Characters"',
+      'In a survey of 4000 online matches for Super Street Fighter IV, I got to be the most popular character, with 16.6% of players choosing my side'
+    ],
+
+    'languages' : [
+      {
+        'sm' : 'gb',
+        'long': 'united-kingdom',
+        'name': 'ENGLISH'
+      },
+      {
+        'sm': 'cn',
+        'long': 'china',
+        'name': 'MANDARIN'
+      },
+      {
+        'sm': 'jp',
+        'long': 'japan',
+        'name': 'JAPANESE'
+      }
+    ],
+
+    'skills' : ['KARATE',
+      'ENERGY ATTACK SHORYUKEN',
+      'WALKING BAREFOOT',
+      'PLAYING VIDEOGAMES DOING',
+      'BURGERS',
+      'MASHING POTATOES',
+      'COPYWRITING ACTING VOICEOVERS'],
+
+    'character' : ['NOBLE',  'DIRECT', 'BOLD', 'CONFIDENT', 'DARING', 'SCHIZOPHRENIC',  'DRAMATIC', 'PERSUASIVE', 'IMMEDIATE', 'WISE'],
+
+    'lifeLesson' : ['You must defeat Sheng Long to stand a chance.'],
+
+    'lifeEducation' : ['To live is to fight and to fight is to live. I started learning fighting at my master Gouken\'s castle, I was beaten at head way too often to remember when exactly.'],
+
+    'likes' : ['VEGETABLES',
+      'BLOOD',
+      'SMALL MARSHMELLOWS',
+      'RED COLOR',
+      'KILLED ENEMY'],
+
+    'dislikes' : ['BISONS',
+      'GHOSTS',
+      'SMARTPHONES',
+      'SMELL OF SEAFOOD',
+      'BARBER',
+      'SHOPS',
+      'SLEEVES'],
+
+    'wants' : ['BE FIT WRITE MORE BOOKS',
+      'FIND A GIRLFRIEND',
+      'TO GO TO CINEMA',
+      'KILL ALL OF AKUMA FAMILY'],
+  };
---
+++ js/functions-elements.js
@@ -0,0 +1,121 @@
+const getContentLiWithYear = (data, part, docSelector, titleSelector) => {
+  const result = data[part]
+    .map(el => {
+      return `<li><p><small>${el.year}</small></p><p>${el.comment}</p></li>`;
+    })
+    .join(" ");
+
+  const docElement = document.querySelector(docSelector);
+  docElement.innerHTML = "";
+  docElement.insertAdjacentHTML("afterbegin", result);
+
+  const titleElement = document.querySelector(titleSelector);
+  titleElement.innerText = data.titles[part];
+};
+
+const getContentLi = (data, part, docSelector, titleSelector) => {
+  const result = data[part]
+    .map(el => {
+      return `<li><p>${el}</p></li>`;
+    })
+    .join(" ");
+
+  const docElement = document.querySelector(docSelector);
+  docElement.innerHTML = "";
+  docElement.insertAdjacentHTML("afterbegin", result);
+
+  const titleElement = document.querySelector(titleSelector);
+  titleElement.innerText = data.titles[part];
+};
+
+const getContentP = (data, part, docSelector, titleSelector) => {
+  const result = `<p>${data[part].join(", ")}</p>`;
+
+  const docElement = document.querySelector(docSelector);
+  docElement.innerHTML = "";
+  docElement.insertAdjacentHTML("afterbegin", result);
+
+  const titleElement = document.querySelector(titleSelector);
+  titleElement.innerText = data.titles[part];
+};
+
+const getContentImageLang = (data, part, docSelector, titleSelector) => {
+  const result = data[part]
+    .map(elm => {
+      return `<span class="language" lang="${elm.sm}">
+<object class="doc-lang-image" data="./images/${elm.sm}.svg" role="img" aria-label="Lang"></object>
+&nbsp;${elm.name}</span>`;
+    })
+    .join(" ");
+
+  const docElement = document.querySelector(docSelector);
+  docElement.innerHTML = "";
+  docElement.insertAdjacentHTML("afterbegin", result);
+
+  const titleElement = document.querySelector(titleSelector);
+  titleElement.innerText = data.titles[part];
+};
+
+const getDocument = (dataResume = resumeRyuEn) => {
+  if (dataResume.experience) {
+    getContentLiWithYear(
+      dataResume,
+      "experience",
+      ".doc-experience",
+      ".title-experience"
+    );
+  }
+  if (dataResume.publications) {
+    getContentLiWithYear(
+      dataResume,
+      "publications",
+      ".doc-publications",
+      ".title-publications"
+    );
+  }
+  if (dataResume.achievements) {
+    getContentLi(
+      dataResume,
+      "achievements",
+      ".doc-achievements",
+      ".title-achievements"
+    );
+  }
+
+  if (dataResume.skills) {
+    getContentP(dataResume, "skills", ".doc-skills", ".title-skills");
+  }
+  if (dataResume.character) {
+    getContentP(dataResume, "character", ".doc-character", ".title-character");
+  }
+  if (dataResume.lifeLesson) {
+    getContentP(
+      dataResume,
+      "lifeLesson",
+      ".doc-life-lesson",
+      ".title-life-lesson"
+    );
+  }
+  if (dataResume.lifeEducation) {
+    getContentP(
+      dataResume,
+      "lifeEducation",
+      ".doc-life-education",
+      ".title-life-education"
+    );
+  }
+  if (dataResume.likes) {
+    getContentP(dataResume, "likes", ".doc-likes", ".title-likes");
+  }
+  if (dataResume.dislikes) {
+    getContentP(dataResume, "dislikes", ".doc-dislikes", ".title-dislikes");
+  }
+  if (dataResume.languages) {
+    getContentImageLang(
+      dataResume,
+      "languages",
+      ".doc-languages",
+      ".title-languages"
+    );
+  }
+};
